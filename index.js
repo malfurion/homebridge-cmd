b2e6f00fc25e1146389e34617b0ee81b2c218af7
Malfurion <malfurion550@gmail.com>
2016-07-24 19:33:51 +0200

    new version

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ var exec = require("child_process").exec;
 module.exports = function(homebridge){
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-cmd-windowcovering", "CMD_WINDOWCOVERING", CmdWindowCoveringAccessory);
+  homebridge.registerAccessory("homebridge-warema", "WAREMA_SHADE", WaremaShadeAccessory);
 }
 
 
-function CmdWindowCoveringAccessory(log, config) {
+function WaremaShadeAccessory(log, config) {
 	this.log = log;
 
 	// url info
 	this.open_cmd   = config["open_cmd"];
 	this.close_cmd  = config["close_cmd"];
-	this.currentState = "opened";
+	this.stop_cmd   = config["stop_cmd"];
+	this.secs       = config["secs"];
+	this.currentState = 100;
 	this.name = config["name"];
 }
 
-CmdWindowCoveringAccessory.prototype = {
+WaremaShadeAccessory.prototype = {
 
 	cmdRequest: function(cmd, callback) {
 		exec(cmd,function(error, stdout, stderr) {
@@ -28,15 +30,15 @@ CmdWindowCoveringAccessory.prototype = {
 
 	setState: function(isOpen, callback) {
 		var cmd;
-
-		if (isOpen) {
+		
+		if (isOpen < currentState) {
 			cmd = this.close_cmd;
 			this.log("Closing Window Covering");
-			this.currentState = "closed"
+			this.currentState = isOpen
 		} else {
 			cmd = this.open_cmd;
 			this.log("Opening Window Covering");
-			this.currentState = "opened"
+			this.currentState = isOpen
 		}
 
 		this.cmdRequest(cmd, function(error, stdout, stderr) {
@@ -46,21 +48,16 @@ CmdWindowCoveringAccessory.prototype = {
 			} else {
 				this.log('CMD function succeeded!');
 				callback();
-				this.log(stdout);
 			}
 		}.bind(this));
 	},
 	
 	getState: function(callback) {
-		if (this.currentState == "opened") {
-			callback(true);
-		} else {
-			callback(false);
-		}
+		callback(null, currentState);
 	},
 	
 	getPosState: function(callback) {
-		callback(Characteristic.PositionState.STOPPED);
+		callback(null, Characteristic.PositionState.STOPPED)
 	},
 
 	identify: function(callback) {
@@ -75,23 +72,27 @@ CmdWindowCoveringAccessory.prototype = {
 		var informationService = new Service.AccessoryInformation();
 
 		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "cmd Manufacturer")
-			.setCharacteristic(Characteristic.Model, "cmd Model")
-			.setCharacteristic(Characteristic.SerialNumber, "cmd Serial Number");
+			.setCharacteristic(Characteristic.Manufacturer, "cmd window covering Manufacturer")
+			.setCharacteristic(Characteristic.Model, "cmd window covering Model")
+			.setCharacteristic(Characteristic.SerialNumber, "cmd window covering Serial Number");
 
 		var windowCoveringService = new Service.WindowCovering(this.name);
 
 		windowCoveringService
 			.getCharacteristic(Characteristic.CurrentPosition)
 			.on('get', this.getState.bind(this));
-		
-		windowCoveringService
-			.getCharacteristic(Characteristic.TargetPosition)
-			.on('set', this.setState.bind(this));
 			
 		windowCoveringService
 			.getCharacteristic(Characteristic.PositionState)
 			.on('get', this.getPosState.bind(this));
+			
+		windowCoveringService
+			.getCharacteristic(Characteristic.TargetPosition)
+			.on('get', this.getState.bind(this));
+		
+		windowCoveringService
+			.getCharacteristic(Characteristic.TargetPosition)
+			.on('set', this.setState.bind(this));
 
 		return [windowCoveringService];
 	}
